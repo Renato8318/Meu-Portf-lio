2b27a5515cf966daf3fbe6343bd49a88b18f2933
Renato Paiva <paivarenato08@gmail.com>
2024-12-29 16:22:08 -0300

    Atualização da sessão sobre, e modificaçoes nos aquivos css e js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Mensagem no console
 console.log("Bem-vindo ao portfólio de Renato Paiva!");
+
+// Adicionando uma saudação para o usuário assim que ele carregar a página
+window.onload = function() {
+    alert("Bem-vindo ao meu portfólio! Explore meus projetos e entre em contato.");
+};
